60eb17c92efed66198329e76433479cd976767ee
martin-dsp <kcs19542001@hotmail.com>
2024-10-25 16:09:40 +0900

    feat: ios-simulator에 띄울 수 있는 최소 설정

--- babel.config.js
+++ babel.config.js
@@ -34,5 +34,8 @@ module.exports = {
       },
     ],
     'react-native-reanimated/plugin',
+    '@babel/plugin-transform-private-methods',
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-private-property-in-object',
   ],
 };
